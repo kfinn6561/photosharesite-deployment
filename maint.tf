21ff12d4492e64ca61775ec8cedb9f03d99baa5c
kieran.finn <kieran.finn@bjss.com>
2022-08-18 09:21:16 +0100

    made general enough to run on multiple machines

--- maint.tf
+++ maint.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file("C:/Users/kiera/GCP_credentials/photosharesite-terraform.json")
+  credentials = file(var.gcp_credential_path)
   project     = var.gcp_project
   region      = var.gcp_region
   zone        = var.gcp_zone
@@ -10,7 +10,7 @@ module "iam" {
   source = "./modules/iam"
 
   gcp_project       = var.gcp_project
-  backend_directory = format("%s/backend/PhotoBackend", var.root_directory)
+  backend_directory = format("%s/%s/PhotoBackend", var.root_directory, var.backend_dir_name)
 }
 
 module "storage" {
@@ -19,8 +19,8 @@ module "storage" {
   gcp_project                   = var.gcp_project
   gcp_region                    = var.gcp_region
   backend_service_account_email = module.iam.backend_service_account_email
-  backend_directory = format("%s/backend/PhotoBackend", var.root_directory)
-  db_directory = format("%s/database", var.root_directory)
+  backend_directory = format("%s/%s/PhotoBackend", var.root_directory, var.backend_dir_name)
+  db_directory = format("%s/%s", var.root_directory, var.db_dir_name)
 }
 
 output "DB-admin-password" {
--- variables.tf
+++ variables.tf
@@ -1,3 +1,10 @@
+variable "gcp_credential_path" {
+  description = "absolute path of the gcp credentials file"
+  type = string
+  default = "C:/Users/kiera/GCP_credentials/photosharesite-terraform.json"
+}
+
+
 variable "gcp_project" {
   description = "gcp Project ID"
   type        = string
@@ -20,4 +27,16 @@ variable "root_directory" {
   description = "root directory"
   type        = string
   default     = "C:/Users/kiera/Dropbox/code/photo_site"
+}
+
+variable "backend_dir_name" {
+  description = "name of the backend directory"
+  type = string
+  default = "backend"
+}
+
+variable "db_dir_name" {
+  description = "name of the database directory"
+  type = string
+  default = "database"
 }
